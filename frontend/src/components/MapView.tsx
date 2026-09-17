e4cfd054afe954a7ed5b7360d742914d626222fd
NKCoder14 <nvk8028@gmail.com>
2026-09-17 21:06:23 +0530

    changed map

--- frontend/src/components/MapView.tsx
+++ frontend/src/components/MapView.tsx
@@ -121,8 +121,8 @@ export const MapView = memo(function MapView({
         zoomControl={true}
       >
         <TileLayer
-          attribution='&copy; <a href="https://carto.com/">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <MapController selectedAssetId={selectedAssetId} nodes={nodes} />
 
